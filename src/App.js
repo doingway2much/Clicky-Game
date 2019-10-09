fb1adaec1647106638ec7ae80539bc06b418ab69
Seth Randell <sethhrandell@gmail.com>
2019-10-09 15:45:29 -0400

    added a ton of style and fpormatting to the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Score from "./components/Score";
-import './App.css';
+import "./App.css";
 import heros from "./cards.json";
 
-
 class App extends Component {
-  // Setting this.state.heros to the cards json array
   state = {
     heros,
     clickedHeroIds: [],
@@ -16,45 +14,54 @@ class App extends Component {
     status: ""
   };
 
-  //shuffle the pup cards in the browser when clicked
   shuffleScoreCard = id => {
     let clickedHeroIds = this.state.clickedHeroIds;
 
-    if(clickedHeroIds.includes(id)){
-      this.setState({ clickedHeroIds: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+    if (clickedHeroIds.includes(id)) {
+      this.setState({
+        clickedHeroIds: [],
+        score: 0,
+        status:
+          "Game Over! Click on any of the images to try your luck again!!!!!"
+      });
       return;
-    }else{
-      clickedHeroIds.push(id)
+    } else {
+      clickedHeroIds.push(id);
 
-      if(clickedHeroIds.length === 10){
-        this.setState({score: 10, status: "You Won! Great Job, Smartie! Click to play again!", clickedHeroIds: []});
-        console.log('You Win');
+      if (clickedHeroIds.length === 10) {
+        this.setState({
+          score: 10,
+          status: "You Won! Awesome, Click to play again!",
+          clickedHeroIds: []
+        });
+        console.log("You Win");
         return;
       }
 
-      this.setState({ heros, clickedHeroIds, score: clickedHeroIds.length, status: " " });
+      this.setState({
+        heros,
+        clickedHeroIds,
+        score: clickedHeroIds.length,
+        status: " "
+      });
 
       for (let i = heros.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [heros[i], heros[j]] = [heros[j], heros[i]];
       }
     }
-  }
+  };
 
-  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Hero Clicky App</h1>
-          <p className="App-intro">
-            Try not to click the same image twice!
-          </p>
+        <div id="App-logo">
+          <h1 className="App-title">MARVEL Hero Guessing game</h1>
+          <p className="App-intro">The object of the game is to click on as many hero's as you can with out clicking on them twice.</p>
+        </div>
         </header>
-        <Score total={this.state.score}
-               goal={10}
-               status={this.state.status}
-               />
+        <Score total={this.state.score} goal={10} status={this.state.status} />
         <Wrapper>
           {this.state.heros.map(Hero => (
             <Card
@@ -66,10 +73,22 @@ class App extends Component {
           ))}
         </Wrapper>
         <footer>
-          <p>Designed and built by Seth Randell. You can find the
-          code<a href="https://github.com/doingway2much/clicky-game" target="_blank" rel="noopener noreferrer"> here</a>.</p>
+        <div className="App-footer">
+          <p>
+            Designed and built by Seth Randell. You can find the code
+            <a
+              href="https://github.com/doingway2much/clicky-game"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              here
+            </a>
+            .
+          </p>
+          </div>
         </footer>
-    </div>
+      </div>
     );
   }
 }
